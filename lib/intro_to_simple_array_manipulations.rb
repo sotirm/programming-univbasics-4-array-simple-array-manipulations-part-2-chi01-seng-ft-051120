422c92a6362f9fca625d3bc98b5c64dd60445163
Michael Sotir <sotirm@umich.edu>
2020-04-30 01:30:19 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)  
+end
+
+def using_insert(arr1, new_elem)
+    arr1.insert(4, new_elem)
+end
+
+def using_uniq(arr1)
+  arr1.uniq  
+end
+
+def using_flatten(arr1)
+    arr1.flatten
+end
+
+def using_delete(arr1, a_string)
+  arr1.delete(a_string)
+end
+
+
+def using_delete_at(arr1, a_int)
+  arr1.delete_at(a_int)  
+end
+
